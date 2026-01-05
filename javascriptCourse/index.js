79b5e465fcb4f0c424ed6f7f1b629913e2c6dc6c
F9563426 Vladimir de Almeida Prado <vladimir@bb.com.br>
2026-01-05 11:31:09 -0300

    Learning about eventListener and adding elements to DOM

--- javascriptCourse/index.js
+++ javascriptCourse/index.js
@@ -19,10 +19,30 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const nomeItem = document.createElement("p");
     nomeItem.innerText = inputItem.value;
 
+    inputCheckbox.addEventListener("click", function() {
+        if (inputCheckbox.checked) {
+            nomeItem.style.textDecoration = "line-through";
+        } else {
+            nomeItem.style.textDecoration = "none";
+        }
+    })
+
     containerItemDaLista.appendChild(inputCheckbox);
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista)
-    listaDeCompras.appendChild(itemDaLista)
 
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"})
+    const data = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"});
+
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+
+    const itemData = document.createElement("p");
+    itemData.classList.add("texto-data");
+    itemData.innerText = dataCompleta;
+    
+    itemDaLista.appendChild(itemData)
+
+    listaDeCompras.appendChild(itemDaLista)
 })
