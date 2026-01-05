5e451d059a4bb95fe88d4979cf769b07b5cf8b6c
vladprado <vladprado.br@gmail.com>
2026-01-05 08:48:03 -0300

    Learning DOM and adding items

---
+++ javascriptCourse/index.js
@@ -0,0 +1,28 @@
+const inputItem = document.getElementById("input-item")
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === "") {
+        alert("Por favor, insira um item!");
+        return
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista)
+    listaDeCompras.appendChild(itemDaLista)
+
+})
