5b109d8c3e85a871ba348f69b4903e54296bb262
F9563426 Vladimir de Almeida Prado <vladimir@bb.com.br>
2026-01-06 07:35:50 -0300

    Finishing modules and course

--- javascriptCourse/index.js
+++ javascriptCourse/index.js
@@ -1,62 +1,17 @@
-const inputItem = document.getElementById("input-item")
+import { criarItemDaLista }  from "./scripts/criarItemDaLista.js";
+import verificarListaVazia from "./scripts/verificarListaVazia.js";
+
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
-let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    if (inputItem.value === "") {
-        alert("Por favor, insira um item!");
-        return
-    }
 
-    const itemDaLista = document.createElement("li");
-    const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement("p");
-    nomeItem.innerText = inputItem.value;
-
-    inputCheckbox.addEventListener("click", function() {
-        if (inputCheckbox.checked) {
-            nomeItem.style.textDecoration = "line-through";
-        } else {
-            nomeItem.style.textDecoration = "none";
-        }
-    })
-
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-
-    itemDaLista.appendChild(containerItemDaLista)
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"})
-    const data = new Date().toLocaleDateString("pt-BR");
-    const hora = new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"});
-
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-
-    const itemData = document.createElement("p");
-    itemData.classList.add("texto-data");
-    itemData.innerText = dataCompleta;
-    
-    itemDaLista.appendChild(itemData)
+    const itemDaLista = criarItemDaLista();
 
     listaDeCompras.appendChild(itemDaLista)
-
-    verificarListaVazia();
+    
+    verificarListaVazia(listaDeCompras);
 })
 
-
-const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
-
-function verificarListaVazia() {
-    const itensDaLista = listaDeCompras.querySelectorAll("li");
-    if (itensDaLista.length === 0) {
-        mensagemListaVazia.style.display = "block";
-    } else {
-        mensagemListaVazia.style.display = "none";
-    }
-}
+verificarListaVazia(listaDeCompras);
