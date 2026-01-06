83b57acf99f2a13da2d19fbd2575b6735f64c233
vladprado <vladprado.br@gmail.com>
2026-01-06 06:58:20 -0300

    creating modules

--- javascriptCourse/index.js
+++ javascriptCourse/index.js
@@ -45,4 +45,18 @@ botaoAdicionar.addEventListener("click", (evento) => {
     itemDaLista.appendChild(itemData)
 
     listaDeCompras.appendChild(itemDaLista)
+
+    verificarListaVazia();
 })
+
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia() {
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if (itensDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block";
+    } else {
+        mensagemListaVazia.style.display = "none";
+    }
+}
