5dee2c6bf7c4833d0b95acc7c0c487dba15c1000
F9563426 Vladimir de Almeida Prado <vladimir@bb.com.br>
2026-01-08 09:44:15 -0300

    Adding final js functions using the form

--- javascriptCourse2/scripts.js
+++ javascriptCourse2/scripts.js
@@ -79,3 +79,50 @@ inputTags.addEventListener("keypress", async (evento) => {
         }
     }
 })
+
+const botaoPublicar = document.querySelector(".botao-publicar");
+
+async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const deuCerto = Math.random() > 0.5;
+
+            if (deuCerto) {
+                resolve("Projeto publicado com sucesso!");
+            } else {
+                reject("Erro ao publicar o projeto.");
+            }
+        }, 2000)
+    })
+}
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    const formulario = document.querySelector("form");
+    formulario.reset();
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "image_projeto.png";
+
+    listaTags.innerHTML = "";
+})
+
+botaoPublicar.addEventListener("click", async (evento) => {
+
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo!");
+    } catch (erro) {
+        alert("Deu tudo errado!");
+    }
+})
