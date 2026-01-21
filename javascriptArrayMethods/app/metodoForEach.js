a30a7fcc5615faaca4d96da742e3e03013522579
F9563426 Vladimir de Almeida Prado <vladimir@bb.com.br>
2026-01-21 13:10:36 -0300

    learning about sort method

--- javascriptArrayMethods/app/metodoForEach.js
+++ javascriptArrayMethods/app/metodoForEach.js
@@ -3,9 +3,10 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+                <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
                 <h2 class="livro__titulo">${livro.titulo}</h2>
                 <p class="livro__descricao">${livro.autor}</p>
                 <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
