4d2c2e33c0c3e6cc79a309fb85fddd9cacba1063
F9563426 Vladimir de Almeida Prado <vladimir@bb.com.br>
2026-01-23 12:00:10 -0300

    Advancing on js and html (7days of code)

--- javascriptArrayMethods/app/metodoFilter.js
+++ javascriptArrayMethods/app/metodoFilter.js
@@ -4,8 +4,28 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => 
-        livro.categoria == categoria
-    )
+    console.log(categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)            
+    console.log(livrosFiltrados)           
     exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+        </div>
+    `
 }
